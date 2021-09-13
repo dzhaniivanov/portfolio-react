b64aad69e61bad66fef8f58c4f16087457f72ac8
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-13 16:17:22 +0300

    adding img icons,work on intro page

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,23 +1,23 @@
-import "./menu.scss";
+import "./Menu.scss";
 
 
-const Menu = () => {
+const Menu = ({ menuOpen, setMenuOpen }) => {
     return (
-        <div className="menu">
+        <div className={"menu " + (menuOpen && "active")}>
             <ul>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#portfolio">Portfolio</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#works">Works</a>
                 </li>
-                <li>
-                    <a href="#testmonials">Testmonials</a>
+                <li onClick={() => setMenuOpen(false)}>
+                    <a href="#testimonials">Testimonials</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
