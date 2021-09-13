f59bcdd9700df6530c2171d6ae813f9cb1c4e541
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-13 14:05:13 +0300

    work on topbar styling

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,15 +1,26 @@
 import "./TopBar.scss"
+import { Person, Mail } from "@material-ui/icons";
 
 const TopBar = () => {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">sssas</a>
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <Person  className="icon"/>
+                        <span>+359 895 62 72 47</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail  className="icon" />
+                        <span>djani.ivanov94@gmail.com</span>
+                    </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+
+                </div>
             </div>
-            
+
         </div>
     )
 }
