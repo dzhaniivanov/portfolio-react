b0967dbb0376c62f3e3e8387a517a5d68cca5588
dzhani ivanov <djani.ivanov94@gmail.com>
2021-09-15 18:24:21 +0300

    styling works component

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -3,6 +3,23 @@ import "./Works.scss";
 const Works = () => {
     return (
         <div className="works" id="works">
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur corrupti maxime fugiat, reprehenderit perferendis accusantium fuga velit ipsa praesentium quae!</p>
+                            </div>
+                            <span>Projects</span>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
